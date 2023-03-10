bbda18d67bd2c910de4b7001ff28f633a0eaf0ed
Mayank303030 <92243054+Mayank303030@users.noreply.github.com>
2023-03-10 14:02:08 +0000

    Update love.php

--- love.php
+++ love.php
@@ -8,7 +8,8 @@
     <?php
       // Display the love story
       include('love-story.php');
-    <p>Julie and Mark had been friends for years, but it wasn't until they both found themselves single that they realized their true feelings for each other. They started dating and fell deeply in love. They enjoyed long walks, candlelit dinners, and talking for hours about anything and everything. They knew that they had found something special, and they vowed to never let it go.</p>
     ?>
+    <p>Julie and Mark had been friends for years, but it wasn't until they both found themselves single that they realized their true feelings for each other. They started dating and fell deeply in love. They enjoyed long walks, candlelit dinners, and talking for hours about anything and everything. They knew that they had found something special, and they vowed to never let it go.</p>
   </body>
 </html>
+
