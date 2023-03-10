d598b20c2603cc5040f37d8b6bf81b79973df93c
Mayank303030 <92243054+Mayank303030@users.noreply.github.com>
2023-03-10 13:44:08 +0000

    Add files via upload

---
+++ horrorstory.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Horror Story</title>
+  </head>
+  <body>
+    <h1>Horror Story</h1>
+    <?php
+      // Display the horror story
+      include('horror-story.php');
+    ?>
+  </body>
+</html>
