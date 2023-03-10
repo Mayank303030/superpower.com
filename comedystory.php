2f91049fd25a87aa5820ecd66458fbf1c7ed4ea9
Mayank303030 <92243054+Mayank303030@users.noreply.github.com>
2023-03-10 13:50:27 +0000

    Update comedystory.php

--- comedystory.php
+++ comedystory.php
@@ -8,6 +8,7 @@
     <?php
       // Display the comedy story
       include('comedy-story.php');
+    <p>John was a shy guy who had never been on a date before. He finally mustered up the courage to ask his crush, Mary, out on a date. He planned the perfect evening, but everything that could go wrong did go wrong. He spilled wine on his shirt, mispronounced the name of the restaurant, and even accidentally called Mary by the wrong name
     ?>
   </body>
 </html>
