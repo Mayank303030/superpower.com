17d2266d59c62f022b028ffb2a8b089ee88e8ec3
Mayank303030 <92243054+Mayank303030@users.noreply.github.com>
2023-03-10 13:51:12 +0000

    Update horrorstory.php

--- horrorstory.php
+++ horrorstory.php
@@ -8,6 +8,7 @@
     <?php
       // Display the horror story
       include('horror-story.php');
+    <p>It was a dark and stormy night. As Samantha walked through the abandoned mansion, she could hear strange noises coming from the shadows. Suddenly, she felt something grab her ankle. She looked down to see a rotting hand reaching out from under the floorboards. She tried to run, but she couldn't escape the grip of the hand. She was dragged down into the darkness, never to be seen again.</p>
     ?>
   </body>
 </html>
