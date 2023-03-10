b26a1e96acf36e22f2522128a0cdebcc7d251d1d
Mayank303030 <92243054+Mayank303030@users.noreply.github.com>
2023-03-10 13:43:40 +0000

    Add files via upload

---
+++ comedystory.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Comedy Story</title>
+  </head>
+  <body>
+    <h1>Comedy Story</h1>
+    <?php
+      // Display the comedy story
+      include('comedy-story.php');
+    ?>
+  </body>
+</html>
